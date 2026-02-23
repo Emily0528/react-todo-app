30621fcef9d79494d46c012fb14f84964f36b0d3
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-23 09:12:07 +0100

    Add form to create new column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
 
@@ -11,14 +12,14 @@ const List = () => {
     { id: 3, title: 'Games', icon: 'gamepad' }
   ]);
 
-  useEffect(() => {
+  const handleSubmit = e => {
+	  e.preventDefault();
+	  setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
 
-        	setTimeout(() => {
-          		setColumns([...columns, { id: 4, title: 'Test column'}]);
-        	}, 2000);
-
-  }, []);
-
+  const [value, setValue] = useState('');
+  
   return (
     <div className={styles.list}>
         <header className={styles.header}>
@@ -28,6 +29,10 @@ const List = () => {
         <section className={styles.columns}>
           {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
         </section>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+          <button>Add column</button>
+        </form>
         
     </div>
   );
