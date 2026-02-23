f614c21fdddeb202355d97491e009d9be5f333f1
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-23 08:36:16 +0100

    Add column creation with setTimeout

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,19 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
 
 const List = () => {
+
+  const columns = [
+  	{ id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ];
+
+  setTimeout(() => {
+    columns.push({ id: 4, title: 'Test column'});
+  }, 2000);
+
   return (
     <div className={styles.list}>
         <header className={styles.header}>
@@ -9,21 +21,7 @@ const List = () => {
         </header>
         <p className={styles.description}>Interesting things I want to check out</p>
         <section className={styles.columns}>
-            <Column
-          title="Books"
-          icon="book"
-        />
-
-        <Column
-          title="Movies"
-          icon="film"
-        />
-
-        <Column
-          title="Games"
-          icon="gamepad"
-        />
-
+          {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
         </section>
         
     </div>
