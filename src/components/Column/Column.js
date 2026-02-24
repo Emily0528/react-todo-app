7d3b93a9b471b1244438093dbdb14aaaa77051f8
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-24 22:56:28 +0100

    Add ability to create new columns and cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,5 +1,6 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 
 const Column = props => {
   return (
@@ -16,6 +17,10 @@ const Column = props => {
               />
             ))}
           </ul>
+          <CardForm 
+            columnId={props.id}
+            action={props.addCard} 
+          />
         </article>
   );
 };
