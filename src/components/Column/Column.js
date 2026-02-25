de494a76098c11d5a1b144b4f9ae7db00cca9ef5
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-25 22:42:35 +0100

    feat: add getFilteredCards selector and use it in Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,17 +2,11 @@ import { useSelector } from 'react-redux';
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { getFilteredCards } from '../../redux/store';
 
 const Column = props => {
 
-  const searchString = useSelector(state => state.searchString.toLowerCase());
-
-  const cards = useSelector(state =>
-    state.cards.filter(card =>
-      card.columnId === props.id &&
-      card.title.toLowerCase().includes(searchString)
-    )
-  );
+  const cards = useSelector(state => getFilteredCards(state, props.id));
   
   return (
         <article className={styles.column}>
