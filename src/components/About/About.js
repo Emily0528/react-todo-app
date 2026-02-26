857290f46fe9eac04939886c31c11cdec1314dbc
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-26 08:37:00 +0100

    Add subpages and the PageTitle component

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import Container from '../Container/Container';
+import PageTitle from '../PageTitle/PageTitle';
+
+const About = () => {
+  return (
+    <Container>
+      <PageTitle>About</PageTitle>
+      <p>This app allows you to manage your tasks using Redux.</p>
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,13 @@
+import Container from '../Container/Container';
+import PageTitle from '../PageTitle/PageTitle';
+
+const Favorite = () => {
+  return (
+    <Container>
+      <PageTitle>Favorite</PageTitle>
+      <p>Here you can find your favorite cards.</p>
+    </Container>
+  );
+};
+
+export default Favorite;
