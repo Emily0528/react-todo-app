c128958dcb2d8db19f47433bff4c6b3bd2d70d81
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-26 22:51:28 +0100

    Add getListById selector

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,27 +2,41 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
-import { getAllColumns } from '../../redux/store';
+import { getAllColumns, getAllLists } from '../../redux/store';
 
 const List = () => {
 
-	const columns = useSelector(getAllColumns);
+  const list = useSelector(state =>
+    getAllLists(state).find(list => list.id === 1)
+  );
+
+  const columns = useSelector(state =>
+    getAllColumns(state).filter(column => column.listId === 1)
+  );
 
   return (
     <div className={styles.list}>
-    <header className={styles.header}>
-      <h2 className={styles.title}>Things to do<span>soon!</span></h2>
-    </header>
-    <p className={styles.description}>Interesting things I want to check out</p>
-    <section className={styles.columns}>
-      {columns.map(column =>
-        <Column
-          key={column.id}
-          {...column}  />
-      )}
-    </section>
-    <ColumnForm />
-  </div>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          {list.title}
+        </h2>
+      </header>
+
+      <p className={styles.description}>
+        {list.description}
+      </p>
+
+      <section className={styles.columns}>
+        {columns.map(column =>
+          <Column
+            key={column.id}
+            {...column}
+          />
+        )}
+      </section>
+
+      <ColumnForm />
+    </div>
   );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,8 @@ export const getFilteredCards = (state, columnId) =>
     card.columnId === columnId &&
     strContains(card.title, state.searchString)
   );
+export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId)
+export const getAllLists = state => state.lists;
 export const getAllColumns = state => state.columns;
 export const addColumn = payload => ({
   type: 'ADD_COLUMN',
