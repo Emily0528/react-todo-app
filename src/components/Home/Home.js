1c91f15443caea2ea4c0a8763a7954336ccb6960
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-28 08:38:27 +0100

    Modify Home to render list of lists

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,21 @@
 import Hero from '../Hero/Hero';
 import SearchForm from '../SearchForm/SearchForm';
-import List from '../List/List';
+import { useSelector } from 'react-redux';
+import { getAllLists } from '../../redux/store';
+//import { Link } from 'react-router-dom';
+//import styles from './Home.module.scss';
+import Lists from '../Lists/Lists';
+
 
 const Home = () => {
+
+  const lists = useSelector(getAllLists);
+
   return (
     <>
       <Hero />
       <SearchForm />
-      <List />
+      <Lists />      
     </>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -12,6 +12,7 @@ export const getListById = ({ lists }, listId) => lists.find(list => list.id ===
 export const getColumnsByList = ({ columns }, listId) =>
   columns.filter(column => column.listId === listId);
 export const getAllLists = state => state.lists;
+//export const getAllLists = ({ lists }) => lists;
 export const getAllColumns = state => state.columns;
 export const addColumn = payload => ({
   type: 'ADD_COLUMN',
