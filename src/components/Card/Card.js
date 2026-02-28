400b6cebd5f2c7e9f70d0744a5ca9aa81a0391db
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-28 15:10:19 +0100

    Add card removal functionality

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import styles from './Card.module.scss';
 import { useDispatch } from 'react-redux';
-import { toggleFavoriteCard } from '../../redux/cardsRedux';
+import { toggleFavoriteCard, removeCard  } from '../../redux/cardsRedux';
 import clsx from 'clsx';
 
 
@@ -14,15 +15,27 @@ const Card = ({ id, title, isFavorite  }) => {
     dispatch(toggleFavoriteCard(id));
   };
 
+  const handleRemove = () => {
+    dispatch(removeCard(id));
+  };
+
   return (
     <div className={styles.card}>
       <span>{title}</span>
-      <span
-        className={clsx(styles.favoriteIcon, isFavorite && styles.favoriteActive)}
-        onClick={handleFavorite}
-      >
-        <FontAwesomeIcon icon={faStarEmpty} />
-      </span>
+      <div className={styles.actions}>
+        <span
+          className={clsx(styles.favoriteIcon, isFavorite && styles.favoriteActive)}
+          onClick={handleFavorite}
+        >
+          <FontAwesomeIcon icon={faStarEmpty} />
+        </span>
+        <span
+          className={styles.deleteIcon}
+          onClick={handleRemove}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </span>
+      </div>
     </div>
   );
 };
--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -11,6 +11,7 @@ export const getAllCards = ({ cards }) => cards;
 const createActionName = actionName => `app/cards/${actionName}`;
 const ADD_CARD = createActionName('ADD_CARD');
 const TOGGLE_CARD_FAVORITE = createActionName('TOGGLE_CARD_FAVORITE');
+const REMOVE_CARD = createActionName('REMOVE_CARD');
 
 
 export const addCard = payload => ({
@@ -23,12 +24,19 @@ export const toggleFavoriteCard = cardId => ({
   payload: cardId
 });
 
+export const removeCard = cardId => ({
+  type: REMOVE_CARD,
+  payload: cardId,
+});
+
 const cardsReducer = (statePart = [], action) => {
   switch(action.type) {
     case 'ADD_CARD':
       return [...statePart, { ...action.payload, id: shortid() }];
     case 'TOGGLE_CARD_FAVORITE':
       return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+    case REMOVE_CARD:
+      return statePart.filter(card => card.id !== action.payload);
     default:
       return statePart;
   };
